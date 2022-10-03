09f55eedde0a0eb2ce6cd6a9013ed54e65696689
dwikifadhilah <dwkfadhilah@gmail.com>
2022-10-03 22:50:00 +0700

    add test data for board feature

--- src/main/java/testdata/BoardData.java
+++ src/main/java/testdata/BoardData.java
@@ -1,9 +1,16 @@
 package testdata;
 
+import com.github.javafaker.Faker;
 import lombok.Getter;
 
 @Getter
 public class BoardData {
+    Faker faker = new Faker();
     private final String listName = "Check-Check";
-    private final String cardName = "Dwk card";
+    private final String cardName = "Dwk test";
+    private final String memberName = "dwiki";
+    private final String filePath = "D:\\images.jpg";
+    private final String notes = faker.food().fruit();
+    private final String labels = faker.country().currency();
+    private final String commentCard = faker.music().genre();
 }
