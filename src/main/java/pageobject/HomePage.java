5988102484debe6113377457e720e77ebec5675a
dwikifadhilah <dwkfadhilah@gmail.com>
2022-10-04 17:59:49 +0700

    change xpath of company select

--- src/main/java/pageobject/HomePage.java
+++ src/main/java/pageobject/HomePage.java
@@ -5,12 +5,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
-    
     public HomePage(WebDriver driver) {
         super(driver);
     }
     
-    @FindBy(xpath = "(//div[@class='MuiBox-root css-1l6cvlp'])[2]")
+    @FindBy(xpath = "(//div[@class='MuiBox-root css-1l6cvlp'])[1]")
     private WebElement selectCompany;
     @FindBy(className = "Card_Card__2LWWW")
     private WebElement selectTeam;
